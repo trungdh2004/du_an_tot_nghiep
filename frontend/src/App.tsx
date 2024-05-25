e7919f59ca5554376f16351b788ca174b58cd483
Do Huu Trung <dohuutrung123456@gmail.com>
2024-05-25 16:15:33 +0700

    base ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainLayout from "./layout/MainLayout";
 import AuthLayout from "./layout/AuthLayout";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<MainLayout />}></Route>
-			<Route path="/auth" element={<AuthLayout />}>
-				<Route path="login" element={<></>} />
-				<Route path="register" element={<></>} />
-			</Route>
-		</Routes>
+		<>
+			<Routes>
+				<Route path="/" element={<MainLayout />}></Route>
+				<Route path="/auth" element={<AuthLayout />}>
+					<Route path="login" element={<></>} />
+					<Route path="register" element={<></>} />
+				</Route>
+			</Routes>
+			<Toaster richColors />
+		</>
 	);
 }
 
