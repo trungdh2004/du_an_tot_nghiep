385374c23eea0d631902597de81542fecbcf1c8a
Do Huu Trung <dohuutrung123456@gmail.com>
2024-05-25 17:21:37 +0700

    format code login register and create page not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,23 @@ import "./App.css";
 import MainLayout from "./layout/MainLayout";
 import AuthLayout from "./layout/AuthLayout";
 import { Toaster } from "./components/ui/sonner";
-import { Login, Register } from "./pages/auth";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<MainLayout />}></Route>
+				<Route path="/" element={<MainLayout />}>
+					<Route path="*" element={<NotFound />}></Route>
+				</Route>
 				<Route path="/auth" element={<AuthLayout />}>
 					<Route path="login" element={<Login />} />
 					<Route path="register" element={<Register />} />
 				</Route>
+
+				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
 			<Toaster richColors />
 		</>
