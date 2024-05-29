7dd11f5e8c9f7d36d858a096c2a5500614833413
Do Huu Trung <dohuutrung123456@gmail.com>
2024-05-29 23:39:38 +0700

    server address

---
+++ server/src/routes/adress.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import authController from "../controllers/auth.controller";
+import addressController from "../controllers/address.controller";
+import authorization from "../middlewares/authentication";
+
+const routerAddress = Router();
+
+routerAddress.get("/paddingAddress", addressController.paddingAddress);
+routerAddress.post("/addAddress", addressController.postAddress);
+routerAddress.delete("/deleteAddress/:id", addressController.deleteAddress);
+
+export default routerAddress;
