df98eb96adbedb7d035752cf78d7a2a2f9723b8e
Do Huu Trung <dohuutrung123456@gmail.com>
2024-05-31 21:29:38 +0700

    base instand

--- frontend/src/pages/clients/Home.tsx
+++ frontend/src/pages/clients/Home.tsx
@@ -1,22 +1,47 @@
-import ButtonCrease from "@/components/ButtonCrease";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+
+import instance from "@/config/instance";
 import useStore from "@/store/home.store";
-import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Home = () => {
+	
 	// const [index, setIndex] = useState(1);
 	const { cart } = useStore();
+	const handleTest =async () => {
+		try {
+			const { data }: any = await instance.get<any>('/address/paddingAddress', {
+				withCredentials:true
+			})
+			
+		} catch (error) {
+			console.log("error", error);
+			
+		}
+	}
+
+	const handleLogin = async () => {
+		try {
+			const { data } = await instance.post("/auth/login", {
+				email:"dohuutrung@gmail.com",
+    			password:"123456"
+			}, {
+				withCredentials: true
+			})
+
+			instance.defaults.headers.common['Authorization'] =`Bearer ${data.accessToken}`;
+		} catch (error) {
+			
+		}
+	}
 
 	return (
-		<>
-			{cart?.map((row) => (
-				<p>
-					{row.name} - {row.quantity}
-				</p>
-			))}
-			<ButtonCrease />
-		</>
+		<div className="px-4 sm:px-6 md:px-[40px] xl:px-[50px] 2xl:px-[60px]">
+			<div className="w-full bg-red-500 min-h-[100vh]">
+				<button onClick={handleTest}>handleTest</button>
+				<button onClick={handleLogin}>Login</button>
+			</div>
+		</div>
 	);
 };
 
