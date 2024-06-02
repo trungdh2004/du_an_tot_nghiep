1d1135e9de5c1db0d8ef5df2bb0e3a46eca66274
Do Huu Trung <dohuutrung123456@gmail.com>
2024-06-02 11:03:39 +0700

    getAddressById

--- server/src/middlewares/authorization.ts
+++ server/src/middlewares/authorization.ts
@@ -1,6 +1,6 @@
-import jwt, { VerifyErrors } from "jsonwebtoken"
-import STATUS from "../utils/status"
-import { NextFunction, Request, Response } from "express"
+import jwt, { VerifyErrors } from "jsonwebtoken";
+import STATUS from "../utils/status";
+import { NextFunction, Request, Response } from "express";
 import UserModel from "../models/User.Schema";
 
 interface PayloadToken {
@@ -9,43 +9,61 @@ interface PayloadToken {
   is_admin: boolean;
 }
 
-const authorization = async (req: Request, res: Response,next:NextFunction) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1]
-        
-        if (!token) {
-            return res.status(STATUS.AUTHENTICATOR).json({
-                message:"Bạn chưa đăng nhập"
-            })
+const authorization = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
+    if (!token) {
+      return res.status(STATUS.AUTHENTICATOR).json({
+        message: "Bạn chưa đăng nhập",
+      });
+    }
+
+    jwt.verify(
+      token,
+      process.env.SECRET_ACCESSTOKEN!,
+      async (err: VerifyErrors | null, data?: object | string) => {
+        if (err) {
+          let message = "Lỗi token";
+
+          if (err.message === "invalid token") {
+            message = "Token không hợp lệ";
+          }
+          if (err.message === "jwt expired") {
+            message = "Token đã hết hạn";
+          }
+
+          return res.status(STATUS.AUTHENTICATOR).json({
+            message: message,
+          });
         }
 
-        jwt.verify(token, process.env.SECRET_ACCESSTOKEN!,async (err:VerifyErrors | null, data?:object | string) => {
-            if (err) {
-                return res.status(STATUS.AUTHENTICATOR).json({
-                    message:"Token đã hết hạn mời bạn đăng nhập lại"
-                })
-            }
+        const existingUser = await UserModel.findById(
+          (data as PayloadToken).id
+        );
 
-            const existingUser = await UserModel.findById((data as PayloadToken).id)
+        if (!existingUser) {
+          return res.status(STATUS.AUTHENTICATOR).json({
+            message: "Tài khoản không thỏa mãn",
+          });
+        }
 
-            if (!existingUser) { 
-                return res.status(STATUS.AUTHENTICATOR).json({
-                    message:"Tài khoản không thỏa mãn"
-                })
-            }
+        if (!existingUser?.is_admin) {
+          return res.status(STATUS.AUTHORIZED).json({
+            message: "Bạn không có quyền",
+          });
+        }
 
-            if (!existingUser?.is_admin) {
-              return res.status(STATUS.AUTHORIZED).json({
-                message: "Bạn không có quyền",
-              });
-            }
-
-            next()
-        });
-    } catch (error:any) {
-        return res.status(STATUS.INTERNAL).json({
-            message: error.message
-        })
-    }
-}
+        next();
+      }
+    );
+  } catch (error: any) {
+    return res.status(STATUS.INTERNAL).json({
+      message: error.message,
+    });
+  }
+};
---
+++ server/src/models/Color.schema.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ColorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ColorModel = mongoose.model("Color", ColorSchema);
+
+export default ColorModel;
