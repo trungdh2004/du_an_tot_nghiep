68353812e5eca69fea9a6a6765d3a899383bedf1
ToiYour <toidz25102004@gmail.com>
2024-06-28 22:52:35 +0700

    #22 Component  Froala Editor

--- frontend/src/config/instance.ts
+++ frontend/src/config/instance.ts
@@ -8,16 +8,20 @@ const instance = axios.create({
 });
 
 const refreshToken = async () => {
-	const response = await axios.post(`${process.env.SERVER_URL}/auth/refreshToken`, {}, {
-		withCredentials:true
-	});
+	const response = await axios.post(
+		`${process.env.SERVER_URL}/auth/refreshToken`,
+		{},
+		{
+			withCredentials: true,
+		},
+	);
 	return response.data.accessToken;
-  };
+};
 
 instance.interceptors.request.use(
 	async function (config) {
 		config.withCredentials = true;
-				
+
 		return config;
 	},
 	function (error) {
@@ -29,31 +33,29 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
 	(response) => {
-		console.log("response", response);
-		
-	  return response;
+		return response;
 	},
 	async (error) => {
-	  const originalRequest = error.config;
-  
-	  // If error is 401 and it's not a retry, attempt to refresh the token
-	  if (error.response.status === 401 && !originalRequest._retry) {
-		originalRequest._retry = true;
-		try {
-		  const newToken = await refreshToken();
-		  instance.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-		  originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-		  return instance(originalRequest);
-		} catch (refreshError) {
-		  // Handle refresh token failure, possibly logging out the user
-		  console.error('Refresh token failed', refreshError);
-		  return Promise.reject(refreshError);
+		const originalRequest = error.config;
+
+		// If error is 401 and it's not a retry, attempt to refresh the token
+		if (error.response.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			try {
+				const newToken = await refreshToken();
+				instance.defaults.headers.common["Authorization"] =
+					`Bearer ${newToken}`;
+				originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+				return instance(originalRequest);
+			} catch (refreshError) {
+				// Handle refresh token failure, possibly logging out the user
+				console.error("Refresh token failed", refreshError);
+				return Promise.reject(refreshError);
+			}
 		}
-	  }
-	  
-	  return Promise.reject(error);
-	}
-  );
-  
+
+		return Promise.reject(error);
+	},
+);
 
 export default instance;
--- frontend/src/pages/clients/Home.tsx
+++ frontend/src/pages/clients/Home.tsx
@@ -1,10 +1,4 @@
-import ProgessBarLoading from "@/components/common/ProgessBarLoading";
-import Quil from "@/components/common/Quil";
-import instance from "@/config/instance";
-import { useAuth } from "@/hooks/auth";
-import { useUploadFile, useMultipleImage } from "@/hooks/upload";
-import useStore from "@/store/home.store";
-import { FormEvent } from "react";
+import { useMultipleImage } from "@/hooks/upload";
 import { useForm } from "react-hook-form";
 
 const Home = () => {
@@ -18,9 +12,7 @@ const Home = () => {
 
 	return (
 		<div className="px-4 sm:px-6 md:px-[40px] xl:px-[50px] 2xl:px-[60px]">
-			<div className="mt-40 h-72">
-				<Quil />
-			</div>
+			<div className="mt-40 h-72">{/* <FroalaEditor /> */}</div>
 			<div className="mt-30">{/* <ProgessBarLoading />; */}</div>
 			<div className="w-full pt-40 bg-green-400 min-h-[100vh]">
 				<form onSubmit={handleSubmit(onSubmit)}>
