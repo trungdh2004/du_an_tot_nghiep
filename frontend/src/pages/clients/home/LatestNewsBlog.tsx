eb7d0d941439ca50fe64e8aea12a58f74b543476
ToiYour <toidz25102004@gmail.com>
2024-07-30 11:35:53 +0700

    #45 Home Client

--- frontend/src/pages/clients/home/LatestNewsBlog.tsx
+++ frontend/src/pages/clients/home/LatestNewsBlog.tsx
@@ -1,8 +1,10 @@
 import { pagingBlogs } from "@/service/blog";
-import { useEffect } from "react";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const LatestNewsBlog = () => {
+	const [newBlogs, setNewBlogs] = useState<IBlogs[]>();
 	useEffect(() => {
 		(async () => {
 			console.log("Đã chạy blog");
@@ -12,7 +14,7 @@ const LatestNewsBlog = () => {
 				fieldSort: "published_at",
 				sort: -1,
 			});
-			console.log(data);
+			setNewBlogs(data?.content);
 		})();
 	}, []);
 	return (
@@ -27,7 +29,10 @@ const LatestNewsBlog = () => {
 					<div className="block flex-shrink-0 flex-grow relative w-full h-0 aspect-w-4 aspect-h-3 rounded-3xl overflow-hidden">
 						<div className="w-full h-full ">
 							<img
-								src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
+								src={
+									newBlogs?.[0]?.thumbnail_url ||
+									"https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
+								}
 								alt=""
 								className="object-cover w-full h-full "
 							/>
@@ -35,30 +40,30 @@ const LatestNewsBlog = () => {
 					</div>
 					<div className=" mt-8 pr-10 flex flex-col">
 						<h2 className="block font-semibold text-neutral-900 dark:text-neutral-100 transition-colors text-lg sm:text-2xl capitalize">
-							turpis cursus in hac habitasse platea dictumst quisque sagittis
-							purus
+							{newBlogs?.[0]?.meta_title}
 						</h2>
 						<span className="hidden sm:block mt-4 text-neutral-500 dark:text-neutral-400">
 							<span className="line-clamp-2">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Obcaecati vero perspiciatis ullam ea? Nihil accusamus similique
-								debitis tempore mollitia? Aperiam.
+								{newBlogs?.[0]?.meta_description}
 							</span>
 						</span>
 						<div className="inline-flex items-center fledx-wrap text-neutral-800 dark:text-neutral-200 text-sm mt-5">
 							<Link
-								to={"/"}
+								to={`/blogs/${newBlogs?.[0]?._id}`}
 								className="flex-shrink-0 relative flex items-center space-x-2"
 							>
 								<div className="wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner rounded-full h-7 w-7 text-sm ring-1 ring-white dark:ring-neutral-900">
 									<img
-										src="https://ciseco-nextjs.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FImage-1.e0d669ee.png&w=128&q=75"
+										src={
+											newBlogs?.[0]?.user?.avatarUrl ||
+											"https://ciseco-nextjs.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FImage-1.e0d669ee.png&w=128&q=75"
+										}
 										alt=""
 										className="absolute inset-0 w-full h-full object-cover rounded-full"
 									/>
 								</div>
 								<span className="block text-neutral-6000 hover:text-black dark:text-neutral-300 dark:hover:text-white font-medium">
-									Anthony Wyat
+									{newBlogs?.[0]?.user?.full_name}
 								</span>
 							</Link>
 							<span className="text-neutral-500 dark:text-neutral-400 mx-[6px] font-medium">
@@ -66,164 +71,70 @@ const LatestNewsBlog = () => {
 							</span>
 							<span className="text-neutral-500 dark:text-neutral-400 font-normal line-clamp-1">
 								{" "}
-								20-12-2024
+								{format(
+									newBlogs?.[0]?.published_at || new Date(),
+									"dd-MM-yyyy",
+								)}
 							</span>
 						</div>
 					</div>
 				</div>
 				<div className="grid gap-6 md:gap-8">
-					<div className="flex">
-						<div className="flex flex-col h-full py-2">
-							<h2 className="block font-semibold text-base">
-								<Link to={""} className="line-clamp-2 capitalize">
-									in mollis nunc sed id semper risus in hendrerit gravida
-								</Link>
-							</h2>
-							<span className="hidden sm:block my-3 text-slate-500 dark:text-slate-400 ">
-								<span className="line-clamp-2">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Deserunt dolorem voluptatibus numquam ut pariatur officiis?
-								</span>
-							</span>
-							<div className="mt-auto hidden sm:block">
-								<div className="inline-flex items-center fledx-wrap text-neutral-800 dark:text-neutral-200 text-sm leading-none">
-									<Link
-										to={""}
-										className="flex-shrink-0 relative flex items-center space-x-2"
-									>
-										<div className="wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner rounded-full h-7 w-7 text-sm ring-1 ring-white dark:ring-neutral-900">
-											<img
-												src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
-												alt=""
-												className="object-cover w-full h-full rounded-3xl"
-											/>
-										</div>
-										<span className="block text-neutral-6000 hover:text-black dark:text-neutral-300 dark:hover:text-white font-medium">
-											Giada Mann
-										</span>
+					{newBlogs?.slice(1).map?.((blog) => (
+						<div className="flex">
+							<div className="flex flex-col h-full py-2">
+								<h2 className="block font-semibold text-base">
+									<Link to={""} className="line-clamp-2 capitalize">
+										{blog?.meta_title}
 									</Link>
-									<span className="text-neutral-500 dark:text-neutral-400 mx-[6px] font-medium">
-										·
-									</span>
-									<span className="text-neutral-500 dark:text-neutral-400 font-normal line-clamp-1">
-										{" "}
-										20-12-2024
-									</span>
+								</h2>
+								<span className="hidden sm:block my-3 text-slate-500 dark:text-slate-400 ">
+									<span className="line-clamp-2">{blog?.meta_description}</span>
+								</span>
+								<div className="mt-auto hidden sm:block">
+									<div className="inline-flex items-center fledx-wrap text-neutral-800 dark:text-neutral-200 text-sm leading-none">
+										<Link
+											to={""}
+											className="flex-shrink-0 relative flex items-center space-x-2"
+										>
+											<div className="wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner rounded-full h-7 w-7 text-sm ring-1 ring-white dark:ring-neutral-900">
+												<img
+													src={
+														blog?.user?.avatarUrl ||
+														"https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
+													}
+													alt=""
+													className="object-cover w-full h-full rounded-3xl"
+												/>
+											</div>
+											<span className="block text-neutral-6000 hover:text-black dark:text-neutral-300 dark:hover:text-white font-medium">
+												{blog?.user?.full_name}
+											</span>
+										</Link>
+										<span className="text-neutral-500 dark:text-neutral-400 mx-[6px] font-medium">
+											·
+										</span>
+										<span className="text-neutral-500 dark:text-neutral-400 font-normal line-clamp-1">
+											{" "}
+											{format(blog?.published_at || new Date(), "dd-MM-yyyy")}
+										</span>
+									</div>
+								</div>
+							</div>
+							<div className="block relative h-full flex-shrink-0 w-2/5 sm:w-1/3 ml-3 sm:ml-5">
+								<div className="absolute inset-0">
+									<img
+										src={
+											blog?.thumbnail_url ||
+											"https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F3579484%2Fpexels-photo-3579484.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600%26lazy%3Dload&w=640&q=75"
+										}
+										alt=""
+										className="object-cover w-full h-full rounded-xl sm:rounded-3xl"
+									/>
 								</div>
 							</div>
 						</div>
-						<div className="block relative h-full flex-shrink-0 w-2/5 sm:w-1/3 ml-3 sm:ml-5">
-							<div className="absolute inset-0">
-								<img
-									src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F3579484%2Fpexels-photo-3579484.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600%26lazy%3Dload&w=640&q=75"
-									alt=""
-									className="object-cover w-full h-full rounded-xl sm:rounded-3xl"
-								/>
-							</div>
-						</div>
-					</div>
-					{/*  */}
-					<div className="flex">
-						<div className="flex flex-col h-full py-2">
-							<h2 className="block font-semibold text-base">
-								<Link to={""} className="line-clamp-2 capitalize">
-									in mollis nunc sed id semper risus in hendrerit gravida
-								</Link>
-							</h2>
-							<span className="hidden sm:block my-3 text-slate-500 dark:text-slate-400 ">
-								<span className="line-clamp-2">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Deserunt dolorem voluptatibus numquam ut pariatur officiis?
-								</span>
-							</span>
-							<div className="mt-auto hidden sm:block">
-								<div className="inline-flex items-center fledx-wrap text-neutral-800 dark:text-neutral-200 text-sm leading-none">
-									<Link
-										to={""}
-										className="flex-shrink-0 relative flex items-center space-x-2"
-									>
-										<div className="wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner rounded-full h-7 w-7 text-sm ring-1 ring-white dark:ring-neutral-900">
-											<img
-												src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
-												alt=""
-												className="object-cover w-full h-full rounded-3xl"
-											/>
-										</div>
-										<span className="block text-neutral-6000 hover:text-black dark:text-neutral-300 dark:hover:text-white font-medium">
-											Giada Mann
-										</span>
-									</Link>
-									<span className="text-neutral-500 dark:text-neutral-400 mx-[6px] font-medium">
-										·
-									</span>
-									<span className="text-neutral-500 dark:text-neutral-400 font-normal line-clamp-1">
-										{" "}
-										20-12-2024
-									</span>
-								</div>
-							</div>
-						</div>
-						<div className="block relative h-full flex-shrink-0 w-2/5 sm:w-1/3 ml-3 sm:ml-5">
-							<div className="absolute inset-0">
-								<img
-									src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F3579484%2Fpexels-photo-3579484.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600%26lazy%3Dload&w=640&q=75"
-									alt=""
-									className="object-cover w-full h-full rounded-xl sm:rounded-3xl"
-								/>
-							</div>
-						</div>
-					</div>
-					{/*  */}
-					<div className="flex">
-						<div className="flex flex-col h-full py-2">
-							<h2 className="block font-semibold text-base">
-								<Link to={""} className="line-clamp-2 capitalize">
-									in mollis nunc sed id semper risus in hendrerit gravida
-								</Link>
-							</h2>
-							<span className="hidden sm:block my-3 text-slate-500 dark:text-slate-400 ">
-								<span className="line-clamp-2">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Deserunt dolorem voluptatibus numquam ut pariatur officiis?
-								</span>
-							</span>
-							<div className="mt-auto hidden sm:block">
-								<div className="inline-flex items-center fledx-wrap text-neutral-800 dark:text-neutral-200 text-sm leading-none">
-									<Link
-										to={""}
-										className="flex-shrink-0 relative flex items-center space-x-2"
-									>
-										<div className="wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner rounded-full h-7 w-7 text-sm ring-1 ring-white dark:ring-neutral-900">
-											<img
-												src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F6168061%2Fpexels-photo-6168061.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260&w=1080&q=75"
-												alt=""
-												className="object-cover w-full h-full rounded-3xl"
-											/>
-										</div>
-										<span className="block text-neutral-6000 hover:text-black dark:text-neutral-300 dark:hover:text-white font-medium">
-											Giada Mann
-										</span>
-									</Link>
-									<span className="text-neutral-500 dark:text-neutral-400 mx-[6px] font-medium">
-										·
-									</span>
-									<span className="text-neutral-500 dark:text-neutral-400 font-normal line-clamp-1">
-										{" "}
-										20-12-2024
-									</span>
-								</div>
-							</div>
-						</div>
-						<div className="block relative h-full flex-shrink-0 w-2/5 sm:w-1/3 ml-3 sm:ml-5">
-							<div className="absolute inset-0">
-								<img
-									src="https://ciseco-nextjs.vercel.app/_next/image?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F3579484%2Fpexels-photo-3579484.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600%26lazy%3Dload&w=640&q=75"
-									alt=""
-									className="object-cover w-full h-full rounded-xl sm:rounded-3xl"
-								/>
-							</div>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</div>
