a23205c1ce074c6ba5bb6862e5a6f3b2340a61ba
Do Huu Trung <dohuutrung123456@gmail.com>
2024-08-11 10:40:40 +0700

    voucher crud

---
+++ server/src/routes/voucher.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import VoucherController from "../controllers/voucher"
+import authentication from "../middlewares/authentication";
+const routerVoucher = Router();
+
+routerVoucher.post("/add", authentication,VoucherController.addVoucher);
+routerVoucher.post("/getCheck", authentication,VoucherController.getVoucherCode);
+routerVoucher.put("/update", authentication,VoucherController.updateVoucher);
+routerVoucher.get("/findOneVoucher/:id", authentication,VoucherController.findOneVoucher);
+routerVoucher.post("/paging", authentication,VoucherController.pagingVoucher);
+routerVoucher.put("/stopAction/:id", authentication,VoucherController.stopAction);
+routerVoucher.put("/startAction/:id", authentication,VoucherController.startAction);
+export default routerVoucher;
