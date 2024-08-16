dec98b22e1b415ff3252c5f0ca8d14730220b679
Do Huu Trung <dohuutrung123456@gmail.com>
2024-08-16 21:26:30 +0700

    shipper

---
+++ frontend/src/layout/ShipperLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShipperLayout = () => {
+  return (
+    <div>ShipperLayout</div>
+  )
+}
+
+export default ShipperLayout
---
+++ frontend/src/routers/ShipperRouter.tsx
@@ -0,0 +1,30 @@
+import MainLayout from "@/layout/MainLayout";
+import Address from "@/pages/clients/address/Address";
+import NotFound from "@/pages/NotFound";
+import UserIndex from "@/pages/admin/users/UserIndex";
+import TestComponent from "@/pages/clients/Test";
+import BlogPage from "@/pages/clients/blogs/BLogPage";
+import HomePage from "@/pages/clients/home/page";
+import BlogDetail from "@/pages/clients/blogs/BlogDetail";
+import DetailProduct from "@/pages/clients/detail-home/page";
+import CartIndex from "@/pages/clients/cart/CartIndex";
+
+const MainRouter = [
+	{
+		path: "/",
+		element: <MainLayout />,
+		children: [
+			{ path: "", element: <HomePage /> },
+			{ path: "/shop/detail/:slug", element: <DetailProduct /> },
+			{ path: "address", element: <Address /> },
+			{ path: "blogs", element: <BlogPage /> },
+			{ path: "blogDetail/:id", element: <BlogDetail /> },
+			{ path: "table", element: <UserIndex /> },
+			{ path: "cart", element: <CartIndex /> },
+			{ path: "*", element: <NotFound /> },
+		],
+	},
+	{ path: "*", element: <NotFound /> },
+
+];
+export default MainRouter;
