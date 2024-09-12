24803f711d177da886336d59d6a7b5eceddcf723
Do Huu Trung <dohuutrung123456@gmail.com>
2024-09-12 16:26:35 +0700

    receivedClientOrder order status

--- server/src/controllers/order/order.controller.ts
+++ server/src/controllers/order/order.controller.ts
@@ -1758,7 +1758,7 @@ class OrderController {
         }
       );
 
-      return res.status(STATUS.BAD_REQUEST).json({
+      return res.status(STATUS.OK).json({
         message: "Cập nhập thành công",
       });
     } catch (error: any) {
