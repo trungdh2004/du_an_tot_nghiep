156ebf32518eb53cc67139eee043cb4d87fcee74
ducnt27 <ducnt2726@gmail.com>
2024-09-18 22:44:37 +0700

    update-v1

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -3,100 +3,101 @@ import { currentAccount } from "@/service/account";
 import { getCountMyShoppingCart, pagingCart } from "@/service/cart";
 import useCart from "@/store/cart.store";
 import {
-	ClientToServerEvents,
-	ServerToClientEvents,
+  ClientToServerEvents,
+  ServerToClientEvents,
 } from "@/types/socket.interface";
 import {
-	createContext,
-	Dispatch,
-	ReactNode,
-	SetStateAction,
-	useEffect,
-	useState,
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
 } from "react";
 import { io, Socket } from "socket.io-client";
 export interface IUser {
-	avatarUrl?: string;
-	blocked_at?: boolean;
-	comment_blocked_at?: boolean;
-	createdAt?: string;
-	email?: string;
-	full_name?: string;
-	is_admin?: boolean;
-	updatedAt?: string;
-	is_staff?: boolean;
-	_id?: string;
-	is_shipper?: boolean;
+  avatarUrl?: string;
+  blocked_at?: boolean;
+  comment_blocked_at?: boolean;
+  createdAt?: string;
+  email?: string;
+  full_name?: string;
+  is_admin?: boolean;
+  updatedAt?: string;
+  is_staff?: boolean;
+  _id?: string;
+  is_shipper?: boolean;
 }
 
 interface AuthContextType {
-	authUser?: IUser | undefined; // thông tin người dùng
-	setAuthUser?: Dispatch<SetStateAction<IUser | undefined>>; // set thông tin người dùng
-	isLoggedIn?: boolean; // trạng thái đăng nhập
-	setIsLoggedIn?: Dispatch<SetStateAction<boolean>>; // set trạng thái đăng nhập
-	socket?: Socket<ServerToClientEvents, ClientToServerEvents>;
+  authUser?: IUser | undefined; // thông tin người dùng
+  setAuthUser?: Dispatch<SetStateAction<IUser | undefined>>; // set thông tin người dùng
+  isLoggedIn?: boolean; // trạng thái đăng nhập
+  setIsLoggedIn?: Dispatch<SetStateAction<boolean>>; // set trạng thái đăng nhập
+  socket?: Socket<ServerToClientEvents, ClientToServerEvents>;
 }
 
 const AuthContext = createContext<AuthContextType>({});
 
 interface AuthProviderProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-	const { setCarts, setTotalCart } = useCart();
-	const [authUser, setAuthUser] = useState<IUser | undefined>(undefined);
-	const [socket, setSocket] =
-		useState<Socket<ServerToClientEvents, ClientToServerEvents>>();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const value = { authUser, setAuthUser, isLoggedIn, setIsLoggedIn, socket };
-	useEffect(() => {
-		setIsLoggedIn(true);
-		(async () => {
-			try {
-				const { data } = await currentAccount();
-				const [carts, totalCountCart] = await Promise.all([
-					pagingCart({ pageSize: 9999999999999 }),
-					getCountMyShoppingCart(),
-				]);
-				setCarts(carts?.data?.data?.content);
-				setTotalCart(totalCountCart?.data?.count);
-				setAuthUser(data?.data);
-				if (data.data._id) {
-					const role = data.data.is_admin || data.data.is_staff;
-					const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-						process.env.SERVER_SOCKET_URL!,
-						{
-							query: {
-								userId: data.data._id,
-								role: role ? "admin" : "user",
-							},
-						},
-					);
-					setSocket(socket);
-					console.log({ socket });
-				}
-			} catch (error) {
-				setTotalCart(0);
-				setCarts([]);
-				setAuthUser(undefined);
-				setIsLoggedIn(false);
-			} finally {
-				setIsLoading(false);
-			}
-		})();
+  const { setCarts, setTotalCart } = useCart();
+  const [authUser, setAuthUser] = useState<IUser | undefined>(undefined);
+  const [socket, setSocket] =
+    useState<Socket<ServerToClientEvents, ClientToServerEvents>>();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const value = { authUser, setAuthUser, isLoggedIn, setIsLoggedIn, socket };
+  console.log("authcontext", authUser)
+  useEffect(() => {
+    setIsLoggedIn(true);
+    (async () => {
+      try {
+        const { data } = await currentAccount();
+        const [carts, totalCountCart] = await Promise.all([
+          pagingCart({ pageSize: 9999999999999 }),
+          getCountMyShoppingCart(),
+        ]);
+        setCarts(carts?.data?.data?.content);
+        setTotalCart(totalCountCart?.data?.count);
+        setAuthUser(data?.data);
+        if (data.data._id) {
+          const role = data.data.is_admin || data.data.is_staff;
+          const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+            process.env.SERVER_SOCKET_URL!,
+            {
+              query: {
+                userId: data.data._id,
+                role: role ? "admin" : "user",
+              },
+            },
+          );
+          setSocket(socket);
+          console.log({ socket });
+        }
+      } catch (error) {
+        setTotalCart(0);
+        setCarts([]);
+        setAuthUser(undefined);
+        setIsLoggedIn(false);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
 
-		return () => {
-			if (socket) {
-				socket.emit("disconnect", authUser?._id);
-			}
-		};
-	}, []);
-	if (isLoading) {
-		return <LoadingFixed />;
-	}
-	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return () => {
+      if (socket) {
+        socket.emit("disconnect", authUser?._id);
+      }
+    };
+  }, []);
+  if (isLoading) {
+    return <LoadingFixed />;
+  }
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export { AuthContext, AuthProvider };
--- frontend/src/pages/clients/account/AccountIndex.tsx
+++ frontend/src/pages/clients/account/AccountIndex.tsx
@@ -1,57 +1,111 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { AuthContext } from '@/contexts/AuthContext'
+import { useForm } from 'react-hook-form'
+import { Input } from '@/components/ui/input'
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 const AccountIndex = () => {
+  const { authUser, setAuthUser } = useContext(AuthContext);
+  const [date, setDate] = useState()
+  // console.log("adadadada", authUser);
+  const form = useForm();
+  useEffect(() => {
+    if (authUser) {
+      form.setValue('full_name', authUser.full_name);
+      form.setValue('email', authUser.email);
+      form.setValue('avatar', authUser.avatarUrl);
+    }
+  }, [authUser, form.setValue])
+  const onSubmit = (data: any) => {
+    console.log("data", data)
+  }
   return (
     <div className='w-full bg-white px-8 '>
       <div className="py-5 border-b border-[#efedec]">
         <h3 className="text-base md:text-lg text-[#333333] font-medium">Hồ Sơ Của Tôi</h3>
         <span className="text-sm md:text-base text-gray-700">Quản lý thông tin hồ sơ để bảo mật tài khoản của bạn</span>
       </div>
-      <div className="pt-8 px-10">
+      <div className="pt-8 pb-12 px-10">
         <form>
-          <div className="flex ">
+          <div className="flex">
             <div className="w-[65%] pr-24">
               <div className="flex items-center pb-10">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Tên đăng nhập</label>
-                <input type="text" className='min-w-[280px] w-full border border-gray-300 rounded-sm px-3 py-2 ' placeholder='Ducnt2323' />
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Tên đăng nhập</Label>
+                <Input type="text" className=' px-3 py-2 ' placeholder='' />
               </div>
               <div className="flex items-center pb-8">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Tên</label>
-                <input type="text" className='min-w-[280px] w-full border border-gray-300 rounded-sm px-3 py-2 ' placeholder='Ducnt2323' />
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Tên</Label>
+                <Input type="text" className=' px-3 py-2 ' placeholder='' {...form.register('full_name')} />
               </div>
               <div className="flex items-center pb-8">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Email</label>
-                <input type="text" className='min-w-[280px] w-full border border-gray-300 rounded-sm px-3 py-2 ' placeholder='Ducnt2323' />
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Email</Label>
+                <Input type="email" readOnly className=' px-3 py-2 ' placeholder='' {...form.register('email')} />
               </div>
               <div className="flex items-center pb-8">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Số điện thoại</label>
-                <input type="text" className='min-w-[280px] w-full border border-gray-300 rounded-sm px-3 py-2 ' placeholder='Ducnt2323' />
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Số điện thoại</Label>
+                <Input type="text" className=' px-3 py-2 ' placeholder='' />
               </div>
               <div className="flex items-center pb-8">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Giới tính</label>
-                <div className="w-full flex items-center gap-x-2">
-                  <div className="">
-                    <input type="radio" name="" id="" className='' />
-                    <label htmlFor="" className='px-2'>Nam</label>
-                  </div>
-                  <div className="">
-                    <input type="radio" name="" id="" className='' />
-                    <label htmlFor="" className='px-2'>Nữ</label>
-                  </div>
-                  <div className="">
-                    <input type="radio" name="" id="" className='' />
-                    <label htmlFor="" className='px-2'>Khác</label>
-                  </div>
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Giới tính</Label>
+                <div className="w-full ">
+                  <RadioGroup defaultValue="option-one" className="w-full flex items-center gap-x-2">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-one" id="option-one" />
+                      <Label htmlFor="option-one">Nam</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-two" id="option-two" />
+                      <Label htmlFor="option-two">Nữ</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-three" id="option-three" />
+                      <Label htmlFor="option-three">Khác</Label>
+                    </div>
+                  </RadioGroup>
                 </div>
               </div>
               <div className="flex items-center pb-8">
-                <label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Ngày sinh</label>
-                <input type="text" className='min-w-[280px] w-full border border-gray-300 rounded-sm px-3 py-2 ' placeholder='Ducnt2323' />
+                <Label className='w-[40%] text-right text-[rgba(85,85,85,.8)] pr-4'>Ngày sinh</Label>
+                <div className="w-full">
+                  {/* <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-full justify-start text-left font-normal",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4 " />
+                        {date ? format(date, "PPP") : <span>Chọn ngày sinh</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover> */}
+                </div>
               </div>
             </div>
             <div className="w-[35%] flex flex-col justify-center items-center border-l border-gray-200 ">
               <div className="size-[100px]">
-                <img src="" className="w-full h-full rounded-full bg-gray-300" alt="" />
+                <img src={authUser?.avatarUrl || ""} className="w-full h-full rounded-full bg-gray-300" alt="" />
               </div>
               <button className='border border-gray-300 rounded-sm px-4 py-2 mt-4 mb-3'>Chọn Ảnh</button>
               <div className="flex flex-col text-[#999] text-sm md:text-base">
@@ -62,7 +116,7 @@ const AccountIndex = () => {
               </div>
             </div>
           </div>
-          <div className="w-[50%] mx-auto">
+          <div className="w-[40%] flex justify-center ">
             <button className='text-white bg-blue-500 px-5 py-2 border rounded-sm'>Lưu</button>
           </div>
         </form>
