4cba55929e38bfac83eac0686d37658f392512d5
Do Huu Trung <dohuutrung123456@gmail.com>
2024-10-05 22:42:41 +0700

    chart dashboard

--- frontend/src/pages/admin/Dashboard/Product.dashboad.tsx
+++ frontend/src/pages/admin/Dashboard/Product.dashboad.tsx
@@ -1,51 +1,58 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
-
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import {
-	ChartConfig,
-	ChartContainer,
-	ChartLegend,
-	ChartLegendContent,
-	ChartTooltip,
-	ChartTooltipContent,
-} from "@/components/ui/chart";
-import { TooltipComponent } from "@/components/common/TooltipComponent";
-import { useQuery } from "@tanstack/react-query";
+import { formatQuantity } from "@/common/localFunction";
 import { getCountProduct } from "@/service/dashboard.service";
-import { optimizeCloudinaryUrl } from "@/common/localFunction";
+import { useQuery } from "@tanstack/react-query";
+import { Bar, BarChart, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
 
-export const description = "A multiple bar chart";
+interface CustomTooltipProps extends TooltipProps<number, string> {}
 
-const chartData = [
-	{ month: "January", desktop: 186, mobile: 80 },
-	{ month: "February", desktop: 305, mobile: 200 },
-	{ month: "March", desktop: 237, mobile: 120 },
-	{ month: "April", desktop: 73, mobile: 190 },
-	{ month: "May", desktop: 209, mobile: 130 },
-	{ month: "June", desktop: 214, mobile: 140 },
-];
+const CustomTooltip: React.FC<CustomTooltipProps> = ({
+	active,
+	payload,
+	label,
+}) => {
+	console.log({ active, payload, label });
 
-const chartConfig = {
-	totalQuantity: {
-		label: "Số lượng",
-		color: "#ff6a00",
-	},
-	totalMoney: {
-		label: "Doanh thu",
-		color: "#008cff",
-	},
-	label: {},
-} satisfies ChartConfig;
+	if (active && payload && payload.length) {
+
+		const url = payload[0]?.payload?.productImage
+		return (
+			<div className="p-2 rounded-sm bg-white box-shadow min-w-[120px]">
+				<p className="font-medium mb-1 text-sm text-center flex justify-center">
+					<img src={url} className="size-5 rounded-sm border"/>
+				</p>
+				<table className="w-full text-xs">
+					<tr>
+						<td colSpan={1}>
+							<div className="size-3 bg-orange-500 rounded-sm "></div>
+						</td>
+						<td className=" text-left" colSpan={2}>
+							<div className="">Số lượng bán</div>
+						</td>
+						<td className="text-end" colSpan={2}>
+							<div className="">{payload[0]?.value}</div>
+						</td>
+					</tr>
+					<tr>
+						<td colSpan={1}>
+							<div className="size-3 bg-blue-500 rounded-sm "></div>
+						</td>
+						<td className=" text-left" colSpan={2}>
+							<div className="">Doanh thu</div>
+						</td>
+						<td className="text-end" colSpan={2}>
+							<div className="">{formatQuantity(payload[1]?.value as number,"đ")}</div>
+						</td>
+					</tr>
+					
+				</table>
+			</div>
+		);
+	}
+
+	return null;
+};
 
 export default function ProductDashboard() {
 	const { data } = useQuery({
@@ -59,81 +66,74 @@ export default function ProductDashboard() {
 	});
 
 	return (
-		<div className="h-[240px] md:h-[300px] lg:h-[400px] flex flex-col p-2">
+		<div className="h-[300px] lg:h-[400px] flex flex-col p-2">
 			<div className="flex items-center justify-between py-2 border-b">
 				<p className="font-semibold">Biểu đồ đơn hàng</p>
 			</div>
 
-			<ChartContainer config={chartConfig} className="flex-1 w-full py-1">
-				<BarChart accessibilityLayer data={data}>
-					<CartesianGrid vertical={false} />
-					<XAxis
-						dataKey="_id"
-						tickLine={false}
-						tickMargin={10}
-						axisLine={false}
-						tick={({ x, y, payload, value }) => {
-							const image = data[payload.index].productImage;
-							return (
-								<foreignObject x={x - 15} y={y - 10} width={30} height={30}>
-									<img
-										src={image}
-										width={30} // Đặt chiều rộng của hình ảnh
-										height={30} // Đặt chiều cao của hình ảnh
-										className="size-[30px] border rounded-sm"
-									/>
-								</foreignObject>
-							);
-						}}
-						// tick={<CustomXAxisLabel />}
-					/>
-					<YAxis
-						dataKey="totalQuantity"
-						yAxisId={0}
-						tickLine={false}
-						label={{
-							value: "Số lượng",
-							angle: -90,
-							position: "insideLeft",
-						}}
-					/>
-					<YAxis
-						dataKey="totalMoney"
-						orientation="right"
-						yAxisId={1}
-						label={{
-							value: "Doanh thu",
-							angle: 90,
-							position: "insideRight",
-						}}
-					/>
-					<ChartTooltip
-						cursor={false}
-						content={
-							<ChartTooltipContent
-								indicator="dashed"
-								labelFormatter={(value,payload) => {
-									return (
-										payload[0] ? <img src={optimizeCloudinaryUrl(payload[0]?.payload?.productImage,20,20)} className="size-5"/> :null
-									)
-								}}
-							></ChartTooltipContent>
-						}
-					/>
-					<Bar
-						dataKey="totalQuantity"
-						fill="var(--color-totalQuantity)"
-						radius={4}
-						yAxisId={0}
-					/>
-					<Bar
-						dataKey="totalMoney"
-						fill="var(--color-totalMoney)"
-						radius={4}
-						yAxisId={1}
-					/>
-				</BarChart>
-			</ChartContainer>
+			<div className="flex-1 w-full">
+				<ResponsiveContainer width={"100%"} height="100%">
+					<BarChart accessibilityLayer data={data}>
+						{/* <CartesianGrid vertical={false} /> */}
+						<Tooltip content={<CustomTooltip />}/>
+						<XAxis
+							dataKey="_id"
+							tickLine={false}
+							tickMargin={10}
+							axisLine={false}
+							tick={({ x, y, payload, value }) => {
+								const image = data[payload.index].productImage;
+								return (
+									<foreignObject x={x - 15} y={y - 10} width={30} height={30}>
+										<img
+											src={image}
+											width={30} // Đặt chiều rộng của hình ảnh
+											height={30} // Đặt chiều cao của hình ảnh
+											className="size-[30px] border rounded-sm"
+										/>
+									</foreignObject>
+								);
+							}}
+						/>
+						<YAxis
+							dataKey="totalQuantity"
+							yAxisId={0}
+							tickLine={false}
+							label={{
+								value: "Số lượng",
+								angle: -90,
+								position: "insideLeft",
+							}}
+							tick={{fontSize:"12px"}}
+						/>
+						<YAxis
+							dataKey="totalMoney"
+							orientation="right"
+							yAxisId={1}
+							label={{
+								value: "Doanh thu",
+								angle: 90,
+								position: "insideRight",
+							}}
+							tick={{fontSize:"12px"}}
+						/>
+						<Bar
+							dataKey="totalQuantity"
+							fill="orange"
+							radius={4}
+							opacity={0.4}
+							yAxisId={0}
+						/>
+						<Bar
+							dataKey="totalMoney"
+							fill="blue"
+							radius={4}
+							yAxisId={1}
+							opacity={1}
+						/>
+					</BarChart>
+				</ResponsiveContainer>
+			</div>
 		</div>
 	);
 }
