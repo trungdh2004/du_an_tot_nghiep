7317928ba8e805a236ee85946471fceede3b6bab
Do Huu Trung <dohuutrung123456@gmail.com>
2024-10-21 23:24:57 +0700

    21-10

--- frontend/src/pages/clients/account/PaymentIndex.tsx
+++ frontend/src/pages/clients/account/PaymentIndex.tsx
@@ -21,7 +21,7 @@ const PaymentIndex = () => {
   
   const [searchObject, setSearchObject] = useState<ISearchObjectPayment>({
     pageIndex: 1,
-    pageSize: 3,
+    pageSize: 6,
   })
   const { data } = useQuery({
     queryKey: ['payment', searchObject],
@@ -51,17 +51,17 @@ const PaymentIndex = () => {
   return (
     <>
       {/* // min-[600px]:col-span-6 min-[900px]:col-span-4 h-[360px] */}
-      <div className="px-10" >
-        <div className="text-[18px] font-medium">Các giao dịch online của bạn</div>
-        <div className="grid grid-cols-12 gap-6 xl:gap-8 mt-5 ">
+      <div className="px-4 md:px-10" >
+        <div className="text-base md:text-lg text-[#333333] font-medium">Các giao dịch online của bạn</div>
+        <div className="grid grid-cols-12 gap-6 mt-5 ">
           {data?.content?.map((item: IPayment, index: number) => {
             return (
-              <div key={index} className="border border-blue-200 bg-gray-100 rounded-[8px] px-4 py-2 col-span-12  min-[900px]:col-span-6 ">
+              <div key={index} className="border border-blue-200 bg-white rounded-[8px] px-4 py-2 col-span-12  min-[900px]:col-span-6 ">
                 <p className="text-sm font-semibold pb-2">Mã đơn hàng: <span className="">{item?.codeOrder}</span></p>
-                <p className="">Có giao dịch thanh toán online với số tiên
+                <p className="">Có giao dịch thanh toán online với số tiền
                   <span className="text-red-500 font-semibold pl-1">{formatQuantity(
                     item?.amount, "₫",)}</span>
-                  , vui long kiểm tra thông tin.</p>
+                  , vui lòng kiểm tra thông tin.</p>
                 <p className="text-gray-500 text-sm text-right pt-2">{format(item?.createdAt || "", "hh:mm dd/MM/yyyy")}</p>
               </div>
             )
