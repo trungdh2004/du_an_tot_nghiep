054385f65f4c065b943f2e7a58064c9200ebcc0e
ducnt27 <ducnt2726@gmail.com>
2024-10-21 21:53:20 +0700

    updated

--- frontend/src/pages/clients/account/Purchase.tsx
+++ frontend/src/pages/clients/account/Purchase.tsx
@@ -10,6 +10,18 @@ import { Link } from "react-router-dom";
 import CancelConfirm from "./CancelConfirm";
 import Evaluate from "./Evaluate";
 import LoadingTable from "./LoadingTable";
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const OrderManagements = () => {
 	const queryClient = useQueryClient();
