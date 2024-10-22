b80fc8d97ed8b1666f81c7ada9ab2d5b82bacca5
ToiYour <toidz25102004@gmail.com>
2024-10-22 16:20:13 +0700

    Save

--- frontend/src/pages/clients/blogs/BlogDetail.tsx
+++ frontend/src/pages/clients/blogs/BlogDetail.tsx
@@ -7,6 +7,8 @@ import { FaRegComment } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa6";
 import { Link, useParams } from "react-router-dom";
 import { Remarkable } from "remarkable";
+import ModalSheetComment from "./comments/ModalSheetComment";
+import { IBlogs } from "@/types/blogs";
 
 const BlogDetail = () => {
 	const { id } = useParams();
@@ -32,14 +34,14 @@ const BlogDetail = () => {
 	const markdownContent = blog?.data?.content;
 	const htmlContent = md.render(markdownContent as any);
 	return (
-		<div className="padding mx-auto py-12">
-			<div className="grid grid-cols-12 gap-5 w-full overflow-visible ">
-				<div className="col-span-3 hidden lg:block pr-5 overflow-visible">
+		<div className="py-12 mx-auto padding">
+			<div className="grid w-full grid-cols-12 gap-5 overflow-visible ">
+				<div className="hidden col-span-3 pr-5 overflow-visible lg:block">
 					<div className="sticky top-0">
-						<h3 className="text-base pb-2 font-medium hidden lg:block">
+						<h3 className="hidden pb-2 text-base font-medium lg:block">
 							{blog?.data?.user_id?.full_name}
 						</h3>
-						<div className="flex  gap-6 pt-2 border-t border-gray-300">
+						<div className="flex gap-6 pt-2 border-t border-gray-300">
 							<div className="flex items-center gap-2 text-[#757575] ">
 								<span>
 									<FaRegHeart size={20} />
@@ -71,7 +73,7 @@ const BlogDetail = () => {
 							<div className="border border-slate-900 w-[50px] h-[50px] rounded-full overflow-hidden">
 								<img
 									src={blog?.data?.user_id?.avatarUrl || "/avatar_25.jpg"}
-									className="w-full h-full object-cover"
+									className="object-cover w-full h-full"
 									alt=""
 								/>
 							</div>
@@ -106,9 +108,7 @@ const BlogDetail = () => {
 							</span>
 						</div>
 						<div className="flex items-center gap-2 ">
-							<span>
-								<FaRegComment size={20} />
-							</span>
+								<ModalSheetComment/>
 							<span className="text-lg font-medium">
 								{blog?.data?.countLike}
 							</span>
@@ -117,27 +117,27 @@ const BlogDetail = () => {
 				</div>
 
 				<div className="col-span-12 lg:col-span-3">
-					<h3 className="text-base pb-2 font-medium mt-4 lg:mt-0 border-b border-gray-300">
+					<h3 className="pb-2 mt-4 text-base font-medium border-b border-gray-300 lg:mt-0">
 						Bài viết khác
 					</h3>
 					<div className="gap-6 mt-2 ">
 						{blog?.orther &&
-							blog?.orther?.map((item: IBlog) => (
+							blog?.orther?.map((item: IBlogs) => (
 								<Link to={`/blogDetail/${item._id}`}>
-									<div className="border rounded-md w-full p-2 flex hover:shadow mt-2">
-										<div className="w-20 h-20   overflow-hidden">
+									<div className="flex w-full p-2 mt-2 border rounded-md hover:shadow">
+										<div className="w-20 h-20 overflow-hidden">
 											<img
 												src={item?.thumbnail_url || "/avatar_25.jpg"}
 												alt=""
-												className="w-full h-full border rounded-sm object-cover"
+												className="object-cover w-full h-full border rounded-sm"
 											/>
 										</div>
-										<div className="flex-1 pl-2 flex flex-col justify-between ">
+										<div className="flex flex-col justify-between flex-1 pl-2 ">
 											<div>
-												<h3 className="line-clamp-1 leading-4 font-medium">
+												<h3 className="font-medium leading-4 line-clamp-1">
 													{item?.meta_title}
 												</h3>
-												<p className="text-sm text-gray-400 line-clamp-2 leading-4 mt-1">
+												<p className="mt-1 text-sm leading-4 text-gray-400 line-clamp-2">
 													{item?.meta_description}
 												</p>
 											</div>
@@ -147,7 +147,7 @@ const BlogDetail = () => {
 													alt=""
 													className="w-6 h-6 border rounded-full"
 												/>
-												<span className="text-xs pl-2">
+												<span className="pl-2 text-xs">
 													{item.user_id.full_name}
 												</span>
 											</div>
---
+++ frontend/src/pages/clients/blogs/comments/Actions.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { GoPencil } from "react-icons/go";
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbDotsVertical } from "react-icons/tb";
+
+interface Props {
+	handleDelete: () => void;
+}
+const Actions = ({ handleDelete }: Props) => {
+	return (
+		<DropdownMenu modal={false}>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant="outline"
+					className="p-0 bg-transparent border-none outline-none hover:bg-transparent focus-visible:right-0"
+				>
+					{" "}
+					<TbDotsVertical />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-auto min-w-[2rem]" align="end">
+				<DropdownMenuItem>
+					<div className="flex items-center gap-2.5 cursor-pointer">
+						<GoPencil /> <span className="text-nowrap">Chỉnh sửa</span>
+					</div>
+				</DropdownMenuItem>
+				<DropdownMenuItem>
+					<div className="flex items-center gap-2.5 cursor-pointer">
+						<HiOutlineTrash /> <span className="text-nowrap" onClick={()=>handleDelete()}>Xoá</span>
+					</div>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default Actions;
