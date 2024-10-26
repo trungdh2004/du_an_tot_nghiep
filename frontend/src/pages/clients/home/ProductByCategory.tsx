cadc4af4ad6eb3d0d5da7a0dccca4c9edf6a8276
Do Huu Trung <dohuutrung123456@gmail.com>
2024-10-26 21:29:06 +0700

    layout home

--- frontend/src/pages/clients/home/ProductByCategory.tsx
+++ frontend/src/pages/clients/home/ProductByCategory.tsx
@@ -5,46 +5,43 @@ import { GrFormNextLink } from "react-icons/gr";
 import { Link } from "react-router-dom";
 import ProductsByCategory from "./ProductsByCategory";
 import { getAllCateActive } from "@/service/category-active";
+import { getAllCate } from "@/service/category-admin";
 type CategoryType = {
 	_id: string;
-	index: number;
-	category: {
-		_id: string;
-		deleted: string;
-		description: string;
-		name: string;
-		slug: string;
-	};
+	deleted: string;
+	description: string;
+	name: string;
+	slug: string;
 };
 const ProductByCategory = () => {
 	const [categorySelect, setCategorySelect] = useState("");
 	const [category, setCategory] = useState<CategoryType[]>();
 	useEffect(() => {
 		(async () => {
-			const { data } = await getAllCateActive();
-			setCategorySelect((data?.data as CategoryType[])[0]?.category?._id);
+			const { data } = await getAllCate();
+			setCategorySelect((data?.data as CategoryType[])[0]?._id);
 			setCategory(data?.data);
 		})();
 	}, []);
 	return (
-		<div className="bg-neutral-100/70 mt-20 py-5">
+		<div className="bg-neutral-50 mt-20 py-8 padding">
 			<div className="flex flex-col items-center justify-center gap-y-12">
-				<h2 className="justify-center text-3xl md:text-4xl 2xl:text-5xl font-semibold   text-neutral-900 dark:text-neutral-50">
-					Bắt đầu khám phá
+				<h2 className="text-2xl font-semibold sm:text-4xl sm:font-bold text-center">
+					Đa dạng mẫu mã sản phẩm
 				</h2>
 				<ScrollArea className="max-sm:w-[300px] whitespace-nowrap rounded-full bg-white box-shadow">
 					<div className="flex w-max space-x-4 p-1">
 						{category?.map((c) => (
 							<div
 								key={c._id}
-								onClick={() => setCategorySelect(c?.category?._id)}
+								onClick={() => setCategorySelect(c?._id)}
 								className={cn(
-									"overflow-hidden  rounded-3xl cursor-pointer px-3.5 py-2 text-sm sm:px-7 sm:py-3 capitalize",
-									categorySelect == c?.category?._id &&
+									"overflow-hidden  rounded-3xl cursor-pointer px-2 py-1 text-sm sm:px-7 sm:py-2 capitalize",
+									categorySelect == c?._id &&
 										"bg-blue-500 text-white",
 								)}
 							>
-								<span>{c?.category?.name}</span>
+								<span>{c?.name}</span>
 							</div>
 						))}
 					</div>
--- frontend/src/pages/clients/home/page.tsx
+++ frontend/src/pages/clients/home/page.tsx
@@ -2,10 +2,12 @@ import Carousel from "./Carousel";
 import DealOfTheDays from "./DealOfTheDays";
 import LatestNewsBlog from "./LatestNewsBlog";
 import ProductByCategory from "./ProductByCategory";
+import Sliderv2 from "./Sliderv2";
 
 const HomePage = () => {
 	return (
 		<div className="">
+			<Sliderv2 />
 			<Carousel />
 			<ProductByCategory />
 			<DealOfTheDays />
