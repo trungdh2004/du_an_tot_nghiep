cd5337618a70e940aeea1e0a4a103c22a9bf40d0
nguyendanhquan6886 <quan23566888@gmail.com>
2024-11-03 15:46:27 +0700

    Save

--- frontend/src/pages/clients/shop/ProductEmpty.tsx
+++ frontend/src/pages/clients/shop/ProductEmpty.tsx
@@ -1,10 +1,9 @@
-import productEmpty from "@/assets/productEmpty.jpg"
 
 const ProductEmpty = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <img src={productEmpty} alt="" className="lg:w-[300px] lg:h-[300px] md:w-[250px] md:h-[250px] w-[200px] h-[200px] object-cover" />
-      <h3 className="lg:text-lg md:text-base text-sm">Không tìm thấy sản phẩm nào theo yêu cầu</h3>
+      <img src={'/no-data-search.png'} alt="" className="lg:w-[300px] lg:h-[300px] md:w-[250px] md:h-[250px] w-[200px] h-[200px] object-cover" />
+      <h3 className="text-sm lg:text-lg md:text-base">Không tìm thấy sản phẩm nào theo yêu cầu</h3>
     </div>
   )
 }
