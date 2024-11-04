630534842393bb248d6fe924ac9dab6c7454beb8
Do Huu Trung <dohuutrung123456@gmail.com>
2024-11-04 23:10:41 +0700

    category and server user and staff

--- server/src/routes/analytics.route.ts
+++ server/src/routes/analytics.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import UserAdmin from "../controllers/admin/user.admin";
+import authorization from "../middlewares/authorization";
+import authentication from "../middlewares/authentication";
 const routerAdmin = Router();
 
 routerAdmin.post("/list-user", UserAdmin.listCurrentUsers);
+routerAdmin.post("/updateRole",authorization, UserAdmin.updateRole);
+routerAdmin.post("/list-user",authentication, UserAdmin.listCurrentUsers);
 
 export default routerAdmin;
