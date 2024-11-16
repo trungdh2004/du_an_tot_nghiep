4384c09c68ef1296a699bf9665feb1ee1559fc71
ducnt27 <ducnt2726@gmail.com>
2024-11-16 23:17:31 +0700

    Voucher-Page-Client done

--- frontend/src/pages/clients/vouchers/VoucherIndex.tsx
+++ frontend/src/pages/clients/vouchers/VoucherIndex.tsx
@@ -1,27 +1,75 @@
 import Coupon from "@/components/common/Coupon/Coupon";
 import Paginations from "@/components/common/Pagination";
+import { getPaginatedVouchersClient } from "@/service/voucher";
+import { SearchObjectType } from "@/types/searchObjecTypes";
+import { typeResponse } from "@/types/typeReponse";
 import { IVoucher } from "@/types/voucher";
-import React, { useState } from "react";
+import { useIsFetching } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
 
 const VoucherIndex = () => {
 	const [vouchers, setVouchers] = useState<IVoucher[]>([]);
 
+	const [searchObject, setSearchObject] = useState<SearchObjectType>({
+		pageIndex: 1,
+		pageSize: 1,
+		keyword: "",
+		fieldSort: "",
+		sort: 1,
+		tab: 1,
+	});
+
+	const [response, setResponse] = useState<typeResponse>({
+		pageCount: 0,
+		totalElement: 0,
+		totalOptionPage: 0,
+	});
+
+	const handleVouchers = async () => {
+		try {
+			const { data } = await getPaginatedVouchersClient(searchObject);
+			setVouchers(data.content);
+			setResponse({
+				pageCount: data.totalPage,
+				totalElement: data.totalAllOptions,
+				totalOptionPage: data.totalOptionPage,
+			});
+			console.log("data", data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		handleVouchers();
+	}, [searchObject]);
+	const handleChangePag = async (value: any) => {
+		console.log("value", value);
+		try {
+			setSearchObject((prev) => ({
+				...prev,
+				pageIndex: value.selected + 1,
+			}));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return (
 		<div>
 			<div className="padding">
 				<div className="py-10">
 					<div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-2  ">
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
-						<Coupon voucher={vouchers} className="w-full" />
+						{vouchers?.map((voucher) => (
+							<div className="" key={voucher?._id}>
+								<Coupon voucher={voucher} className="w-full" />
+							</div>
+						))}
 					</div>
 					<div className="flex justify-center pt-8">
-						<Paginations />
+						<Paginations
+							forcePage={searchObject.pageIndex - 1}
+							pageCount={response.pageCount}
+							handlePageClick={handleChangePag}
+						/>
 					</div>
 				</div>
 			</div>
