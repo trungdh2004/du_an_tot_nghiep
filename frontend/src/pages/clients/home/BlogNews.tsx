51789d04937dcf7411a50b04ee028ff5ff517292
nguyendanhquan6886 <quan23566888@gmail.com>
2024-11-18 10:35:04 +0700

    View Rating Component Product

---
+++ frontend/src/pages/clients/home/BlogNews.tsx
@@ -0,0 +1,154 @@
+
+const BlogNews = () => {
+  return (
+    <section className="md:mt-4 ">
+  <div className="wraper">
+    <div className="sortable-gallery">
+      <div className="gallery-filters" />
+      <div className="m-[0_-7.5px] gallery-container clearfix">
+        <div className="flex w-[50%] float-left p-[0_7.5px_15px] col:p-0 col:mb-4 lg:block  lg:w-full">
+          <div
+            className="relative rounded-[15px] overflow-hidden z-10 group before:absolute before:left-0 before:top-0 before:w-full
+                          before:h-full before:bg-[rgba(7,7,7,0.4)] before:rounded-[15px] before:z-20"
+          >
+            <img
+              src="https://wpolive.com/html/bloggar-tailwind/assets/images/hero/img-1.jpg"
+              alt=""
+              className="img img-responsive w-full rounded-[15px] scale-[1] transition-all group-hover:scale-[1.3]"
+            />
+            <div className="absolute left-0 bottom-0 p-[30px] z-20 col:p-3">
+              <div
+                className="p-[3px_15px] text-center text-xl rounded-[5px] inline-block font-bold text-[#003aae] 
+                              bg-[rgba(255,255,255,0.8)] col:text-base"
+              >
+                Fasion
+              </div>
+              <h2 className="text-[35px] text-white mt-[15px] leading-10 col:text-xl font-semibold">
+                <a href="blog-single.html">
+                  What’s In Trend In Women's Fashion Summer?
+                </a>
+              </h2>
+              <p className="text-white my-[15px] col:text-[14px] ">
+                You can customize the view Blog posts with a simple mouse click
+                and immediately see the result of your changes.{" "}
+              </p>
+              <ul className="flex items-center">
+                <li className="relative text-white mr-[20px] ">
+                  <img
+                    src="assets/images/blog/blog-avater/img-1.jpg"
+                    alt=""
+                    className="w-[40px] h-[40px] rounded-[50%]"
+                  />
+                </li>
+                <li className="relative text-white ">
+                  By <a href="blog-single.html">Robert</a>
+                </li>
+                <li className="relative text-white ml-[20px] pl-[20px] before:absolute before:left-[-8px] before:top-[25%] before:w-[8px] before:h-[8px] before:bg-white before:rounded-[50%] before:translate-y-1/2">
+                  20 Nov 2022
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[50%] float-left p-[0_7.5px_15px] col:p-0 col:mb-4 lg:block lg:w-full">
+          <div
+            className="relative rounded-[15px] overflow-hidden z-10 group before:absolute before:left-0 before:top-0 before:w-full
+                          before:h-full before:bg-[rgba(7,7,7,0.4)] before:rounded-[15px] before:z-20"
+          >
+            <img
+              src="https://wpolive.com/html/bloggar-tailwind/assets/images/hero/img-2.jpg"
+              alt=""
+              className="img img-responsive w-full rounded-[15px] scale-[1] transition-all group-hover:scale-[1.3]"
+            />
+            <div className="absolute left-0 bottom-0 p-[30px] z-20 col:p-3">
+              <div
+                className="p-[3px_15px] text-center text-xl rounded-[5px] inline-block font-bold text-[#003aae] 
+                              bg-[rgba(255,255,255,0.8)] col:text-base"
+              >
+                Travel
+              </div>
+              <h2 className="text-[35px] text-white mt-[15px] leading-10 col:text-xl font-semibold col:mt-[10px]">
+                <a href="blog-single.html">
+                  Traveling Makes You More Interesting
+                </a>
+              </h2>
+              <ul className="flex items-center">
+                <li className="relative text-white ">
+                  By <a href="blog-single.html">Robert</a>
+                </li>
+                <li className="relative text-white ml-[20px] pl-[20px] before:absolute before:left-[-8px] before:top-[25%] before:w-[8px] before:h-[8px] before:bg-white before:rounded-[50%] before:translate-y-1/2">
+                  20 Nov 2022
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[50%] p-[0_7.5px_15px] col:p-0 justify-between lg:w-full m-[0_-5px] pr-0 sm:m-0 sm:pr-[7.5px] sm:block">
+          <div
+            className="relative mx-[7px] sm:mx-0 sm:mb-4 rounded-[15px] overflow-hidden z-10 group before:absolute before:left-0 before:top-0 before:w-full
+                          before:h-full before:bg-[rgba(7,7,7,0.4)] before:rounded-[15px] before:z-20"
+          >
+            <img
+              src="https://wpolive.com/html/bloggar-tailwind/assets/images/hero/img-3.jpg"
+              alt=""
+              className="img img-responsive w-full rounded-[15px] scale-[1] transition-all group-hover:scale-[1.3]"
+            />
+            <div className="absolute left-0 bottom-0 p-[30px] xl:p-[25px] z-20 col:p-2">
+              <div
+                className="p-[3px_15px] text-center text-xl rounded-[5px] inline-block font-bold text-[#003aae] 
+                              bg-[rgba(255,255,255,0.8)] col:text-base"
+              >
+                Food
+              </div>
+              <h2 className="text-[28px] text-white mt-[15px]  font-semibold col:text-xl">
+                <a href="blog-single.html">Top 10 Healthy Food</a>
+              </h2>
+              <ul className="flex items-center">
+                <li className="relative text-white ">
+                  By <a href="blog-single.html">Robert</a>
+                </li>
+                <li className="relative text-white ml-[20px] pl-[20px] before:absolute before:left-[-8px] before:top-[25%] before:w-[8px] before:h-[8px] before:bg-white before:rounded-[50%] before:translate-y-1/2">
+                  20 Nov 2022
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div
+            className="relative mx-[7px] sm:mx-0 sm:mb-4 rounded-[15px] overflow-hidden z-10 group before:absolute before:left-0 before:top-0 before:w-full
+                          before:h-full before:bg-[rgba(7,7,7,0.4)] before:rounded-[15px] before:z-20"
+          >
+            <img
+              src="https://wpolive.com/html/bloggar-tailwind/assets/images/hero/img-3.jpg"
+              alt=""
+              className="img img-responsive w-full rounded-[15px] scale-[1] transition-all group-hover:scale-[1.3]"
+            />
+            <div className="absolute left-0 bottom-0 p-[30px] xl:p-[25px] z-20 col:p-2">
+              <div
+                className="p-[3px_15px] text-center text-xl rounded-[5px] inline-block font-bold text-[#003aae] 
+                              bg-[rgba(255,255,255,0.8)] col:text-base"
+              >
+                Business
+              </div>
+              <h2 className="text-[28px] text-white mt-[15px]  font-semibold col:text-xl">
+                <a href="blog-single.html">What Makes a Leader?</a>
+              </h2>
+              <ul className="flex items-center">
+                <li className="relative text-white ">
+                  By <a href="blog-single.html">Robert</a>
+                </li>
+                <li className="relative text-white ml-[20px] pl-[20px] before:absolute before:left-[-8px] before:top-[25%] before:w-[8px] before:h-[8px] before:bg-white before:rounded-[50%] before:translate-y-1/2">
+                  20 Nov 2022
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default BlogNews
